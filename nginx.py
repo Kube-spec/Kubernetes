14b5e0242686dffed53082d508c0dbf79c582e7b
Kube-spec <aniket11980@gmail.com>
2024-01-10 12:12:32 +0000

    Added nginx and test.py to master

---
+++ nginx.py
@@ -0,0 +1,3 @@
+1. NGINX is a simple Web server.
+2. NGINX is often used as a reverse proxy, which means it forwards request from clients to backend servers. This is very useful for load balancing and distributing incoming requests across multiple application servers, improving the overall performance and reliability of web applications.
+3. Loadbalancing: NGINX can distribute incoming traffic across multiple backend servers ensures that no single server becomes overwhelmed with requests. This helps optimize resource utilization and prevents downtime due to server overload
---
+++ test.py
@@ -0,0 +1,4 @@
+date
+echo $SHELL
+PWD
+echo "Hello world"
